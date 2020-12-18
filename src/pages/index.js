2a8b7710e2820eb7990da5b896dba652a5a09436
MGhebreslassie <messeleghebreslassie@gmail.com>
2020-12-18 16:30:56 -0700

    watch the video

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,16 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Learn to design and code React apps</h1>
+    <p>Complete courese about the best tools and design systems.
+      Protoype and build apps with React and Swift.
+    </p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <Link to="/using-typescript/">Watch the video</Link>
   </Layout>
 )
 
